b0da95ae0bd4934a3d9d0187365b214a424c8bd8
chenjian <chenjian@erayt.com>
2018-05-10 22:15:13 +0800

    【提交】评论新增代码，测试innerhtml功能

--- src/component/ComDetail.js
+++ src/component/ComDetail.js
@@ -36,6 +36,15 @@ class ComDetail extends Component {
     })
   
   }
+  _getProcessedContent(content){
+    return content
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&#039;")
+      .replace(/`([\S\s]+?)`/g, '<code>$1</code>')
+  }
 
   handleDeleteComment(){
     if (this.props.onDeleteComment) {
@@ -49,7 +58,9 @@ class ComDetail extends Component {
       <div className='comment-user'>
         <span>{this.props.comment.username} </span>：
       </div>
-      <p>{this.props.comment.content}</p>
+      <p dangerouslySetInnerHTML={{
+        __html:this._getProcessedContent(this.props.comment.content)
+      }}></p>
       <span className='comment-createdtime'>
           {this.state.timeString}
       </span>
